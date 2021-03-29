167a79f7667e9103ed9e3a714569615510be981a
kbbushman <k.bushman@yahoo.com>
2021-03-28 23:55:09 -0700

    Adds input transpiling with esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,45 @@
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
+import { useState, useEffect, useRef } from 'react';
 
 const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  const handleClick = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  };
+
   return (
     <div>
       <h1>JBook</h1>
+      <textarea
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 };
