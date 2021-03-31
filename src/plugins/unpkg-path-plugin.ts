7f03743fd4be7aaae098bccb8382214e0cf28979
kbbushman <k.bushman@yahoo.com>
2021-03-30 23:53:36 -0700

    Adds esbuild plugin for library requests to unkpkg.com

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import * as esbuild from 'esbuild-wasm';
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResole', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer + '/').href,
+          };
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import message from 'tiny-test-pkg';
+              console.log(message);
+            `,
+          };
+        }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
+      });
+    },
+  };
+};
