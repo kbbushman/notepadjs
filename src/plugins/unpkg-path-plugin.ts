bcdbd2005a78bdf78f64557a6737847870c71034
kbbushman <k.bushman@yahoo.com>
2021-04-02 19:00:25 -0700

    Replaces hard coded build content with codeInput from user

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localForage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -38,11 +38,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React from 'react';
-              import ReactDOM from 'react-dom';
-              console.log(React, ReactDOM);
-            `,
+            contents: inputCode,
           };
         }
 
