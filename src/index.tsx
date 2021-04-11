5f7b00e5adf057a383d53929ae864a89c83babfa
kbbushman <k.bushman@yahoo.com>
2021-04-10 20:15:53 -0700

    Adds iframe with eval in srcDoc for rendering user input

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -34,10 +35,23 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -50,6 +64,7 @@ const App = () => {
         <button onClick={handleClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe title="code" sandbox="allow-scripts" ref={iframe} srcDoc={html} />
     </div>
   );
 };
