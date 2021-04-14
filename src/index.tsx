bbc120590d768a982f0c19f4f7f667f9bedbedaf
kbbushman <k.bushman@yahoo.com>
2021-04-13 18:35:16 -0700

    Adds bulmaswatch superhero css theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
