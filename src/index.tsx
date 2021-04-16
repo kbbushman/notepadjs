c0177891d9ec781900a2da6c9515cc8dc83c359c
kbbushman <k.bushman@yahoo.com>
2021-04-15 19:03:33 -0700

    Refactors user input bundling to bundler module

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,17 @@
-import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
   const handleClick = async () => {
-    if (!ref.current) return;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
