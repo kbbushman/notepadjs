781cb29688752167f6b83d4e63b8cd97835c0e6a
kbbushman <k.bushman@yahoo.com>
2021-04-15 20:05:06 -0700

    Refactors code editor and state to code cell module

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,13 @@
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const handleClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
       <h1>JBook</h1>
-      <CodeEditor initialValue="" onChange={(value) => setInput(value)} />
-      <div>
-        <button onClick={handleClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
