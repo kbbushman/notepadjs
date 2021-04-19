1fa9c8d071c1f4a3ffed0dbd736e9e7860166a29
kbbushman <k.bushman@yahoo.com>
2021-04-18 22:26:37 -0700

    Adds error state for bundling errors

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,11 +7,13 @@ import Resizable from './resizable';
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
+  const [err, setErr] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 1000);
 
     return () => {
@@ -25,7 +27,7 @@ const CodeCell = () => {
         <Resizable direction="horizontal">
           <CodeEditor initialValue="" onChange={(value) => setInput(value)} />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   );
