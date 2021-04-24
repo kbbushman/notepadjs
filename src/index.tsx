cdba8609ec29d54dc3a7a54d3cf22ba92758beaa
kbbushman <k.bushman@yahoo.com>
2021-04-23 19:09:21 -0700

    Adds react-redux provider with store

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 // import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
