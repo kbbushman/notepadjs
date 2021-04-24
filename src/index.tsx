86f8e4d3a3337a3ebfd6a6d5a2c549bb5a6427a6
kbbushman <k.bushman@yahoo.com>
2021-04-24 14:15:37 -0700

    Adds cell list, removes text cell

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
+import CellList from './components/cell-list';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-// import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
