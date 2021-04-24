9e4e75d81e443e5210a56aefead50a22ac31278f
kbbushman <k.bushman@yahoo.com>
2021-04-24 15:39:39 -0700

    Adds font-awesome css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/cell-list';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
   return (
