c84e0c0820db5317ddfa5ea4304be765403f0780
kbbushman <k.bushman@yahoo.com>
2021-04-23 23:33:30 -0700

    Adds insert cell before reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -33,12 +33,43 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
       return;
     case ActionType.INSERT_CELL_BEFORE:
-      return state;
+      const cell: Cell = {
+        content: '',
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+
+      return;
     default:
       return state;
   }
 });
+
+const randomId = () => {
+  // Base36 = string with numbers and letters
+  return Math.random().toString(36).substr(2, 5);
+};
 
 export default reducer;
