6ffe7db96fb8d5f1129f2de8a56b4fbbf37f263f
kbbushman <k.bushman@yahoo.com>
2021-04-25 14:25:39 -0700

    Refactors show method to prevent execution in later cells

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,26 +18,32 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [
-      `
-        import _React from 'react';
-        import _ReactDOM from 'react-dom';
-        const show = (value) => {
-          const root = document.getElementById('root');
-          if (typeof value === 'object') {
-            if (value.$$typeof && value.props) {
-              _ReactDOM.render(value, root);
-            } else {
-              root.innerHTML = JSON.stringify(value);
-            }
+    const showFunc = `
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+      var show = (value) => {
+        const root = document.getElementById('root');
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
           } else {
-            root.innerHTML = value;
+            root.innerHTML = JSON.stringify(value);
           }
-        };
-      `,
-    ];
+        } else {
+          root.innerHTML = value;
+        }
+      };
+    `;
+
+    const showFuncNoop = 'var show = () => {}';
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
