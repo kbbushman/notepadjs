898c0f9371cfeea16b2bb368cbc3134c2b73df6d
kbbushman <k.bushman@yahoo.com>
2021-04-24 17:52:51 -0700

    Adds add code/text cell with actions

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,19 @@
+import { useActions } from '../hooks/use-actions';
+import './add-cell.css';
+
+interface AddCellProps {
+  nextCellId: string;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+  const { insertCellBefore } = useActions();
+
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+    </div>
+  );
+};
+
+export default AddCell;
