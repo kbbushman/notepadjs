51adc4bff32b055215c623d63d364847e9f79c17
kbbushman <k.bushman@yahoo.com>
2021-04-27 00:13:42 -0700

    Adds local-api serve method

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,10 @@
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run the server on', '4005')
-  .action((filename = 'notebook.js', options) => {
-    console.log(filename, options);
+  .action((filename = 'notebook.js', options: { port: string }) => {
+    serve(parseInt(options.port), filename, '/');
   });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server running...');
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log('Serving traffic on port ', port);
+  console.log('Saving/fetching cells from ', filename);
+  console.log('The file is in dir ', dir);
 };
