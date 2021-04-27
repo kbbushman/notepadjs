7118fefff1adb1da9bb55773d7f6f1157e5fd07e
kbbushman <k.bushman@yahoo.com>
2021-04-27 00:23:07 -0700

    Adds filename/path args to serve

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
 
@@ -6,5 +7,6 @@ export const serveCommand = new Command()
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run the server on', '4005')
   .action((filename = 'notebook.js', options: { port: string }) => {
-    serve(parseInt(options.port), filename, '/');
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
