dfcbcb1d387b440da454f4dbfcada3541d6fb846
kbbushman <k.bushman@yahoo.com>
2021-04-27 18:44:04 -0700

    Adds express app with listener

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,9 @@
+import express from 'express';
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log('Serving traffic on port ', port);
-  console.log('Saving/fetching cells from ', filename);
-  console.log('The file is in dir ', dir);
+  const app = express();
+
+  app.listen(port, () => {
+    console.log('Listening on port', port);
+  });
 };
