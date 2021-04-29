d799e71916395e992fc18eabfd71b41acde0968d
kbbushman <k.bushman@yahoo.com>
2021-04-28 18:24:56 -0700

    Adds post cells route handler

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,10 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
 import express from 'express';
-const router = express.Router();
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+
+  const fullPath = path.join(dir, filename);
+
   router.get('/cells', async (req, res) => {});
 
-  router.post('/cells', async (req, res) => {});
+  router.post('/cells', async (req, res) => {
+    const { cells }: { cells: Cell[] } = req.body;
+
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
+  });
 
   return router;
 };
