77950b190d060c76ac07b9b780aaa52636abd090
kbbushman <k.bushman@yahoo.com>
2021-04-30 19:43:56 -0700

    Adds save cells action creator

--- packages/local-client/src/state/action-creators/index.ts
+++ packages/local-client/src/state/action-creators/index.ts
@@ -11,6 +11,7 @@ import {
 } from '../actions';
 import { Cell, CellTypes } from '../cell';
 import bundle from '../../bundler';
+import { RootState } from '../reducers';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -87,6 +88,25 @@ export const fetchCells = () => {
     } catch (err) {
       dispatch({
         type: ActionType.FETCH_CELLS_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {
+      cells: { data, order },
+    } = getState();
+
+    const cells = order.map((id) => data[id]);
+
+    try {
+      await axios.post('/cells', { cells });
+    } catch (err) {
+      dispatch({
+        type: ActionType.SAVE_CELLS_ERROR,
         payload: err.message,
       });
     }
