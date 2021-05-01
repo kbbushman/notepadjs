9b999b335682c0a26d5e76e7daf44b7f2d05aea2
kbbushman <k.bushman@yahoo.com>
2021-04-30 20:19:20 -0700

    Adds fetchCells to useEffect dependencies

--- packages/local-client/src/components/cell-list.tsx
+++ packages/local-client/src/components/cell-list.tsx
@@ -13,7 +13,7 @@ const CellList: React.FC = () => {
 
   useEffect(() => {
     fetchCells();
-  }, []);
+  }, [fetchCells]);
 
   const renderedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
