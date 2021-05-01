22913927df584047ff712f3fda3b24aec7ee1b3a
kbbushman <k.bushman@yahoo.com>
2021-04-30 23:26:20 -0700

    Updates local dependency names with organization

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Command } from 'commander';
-import { serve } from 'local-api';
+import { serve } from '@notepadjs/local-api';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
