87934b725ed2b9688001ae197531ca504ecea4ba
kbbushman <k.bushman@yahoo.com>
2021-04-30 19:56:46 -0700

    Adds fetchCells on mount

--- packages/local-client/src/components/cell-list.tsx
+++ packages/local-client/src/components/cell-list.tsx
@@ -1,13 +1,19 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
+import { useActions } from '../hooks/use-actions';
 import './cell-list.css';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
     return order.map((id) => data[id]);
   });
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, []);
 
   const renderedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
